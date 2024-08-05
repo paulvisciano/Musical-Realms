c974e3d129ab13a65fba62711f121696fd020588
Paul Visciano <paulvisciano.dev@gmail.com>
2024-08-05 00:46:01 -0400

    Latest tracks

--- src/pages/realms/musicalCubes/tracks.ts
+++ src/pages/realms/musicalCubes/tracks.ts
@@ -1,25 +1,21 @@
 
-export interface Track { 
-  name : string; 
-  sounds : string[];
+export interface Track {
+  name: string;
+  sounds: string[];
 }
 
-const tracks : Track[] = [
-      { 
-        name : "Best Feelings",
-        sounds : ["./assets/sounds/musicalCube/tracks/Best Feelings/melody/001_AHA_SV_75_piano_loop_day_to_day_F.wav","./assets/sounds/musicalCube/tracks/Best Feelings/melody/002_AHA_SV_75_keys_loop_best_feelings_F.wav","./assets/sounds/musicalCube/tracks/Best Feelings/melody/003_AHA_SV_75_brass_loop_best_feelings_F.wav","./assets/sounds/musicalCube/tracks/Best Feelings/melody/004_AHA_SV_75_vocal_loop_best_feelings_F.wav","./assets/sounds/musicalCube/tracks/Best Feelings/one_shots/AHA_SV_brass_stack_one_shot_phrase_runner_up_F.wav"]
-      },
-      { 
-        name : "Daydreamer",
-        sounds : ["./assets/sounds/musicalCube/tracks/Daydreamer/melody/001_AHA_SV_70_keys_loop_daydreamer_Gm.wav","./assets/sounds/musicalCube/tracks/Daydreamer/melody/002_AHA_SV_70_guitar_stack_loop_daydreamer_Gm.wav","./assets/sounds/musicalCube/tracks/Daydreamer/melody/003_AHA_SV_70_brass_stack_loop_daydreamer_Gm.wav","./assets/sounds/musicalCube/tracks/Daydreamer/melody/004_AHA_SV_70_vocal_loop_daydreamer_Gm.wav"]
-      },
-      { 
-        name : "In the City",
-        sounds : ["./assets/sounds/musicalCube/tracks/In the City/melody/001_AHA_SV_60_piano_loop_in_the_city_6-8_Am.wav","./assets/sounds/musicalCube/tracks/In the City/melody/002_AHA_SV_60_guitar_stack_loop_in_the_city_6-8_Am.wav","./assets/sounds/musicalCube/tracks/In the City/melody/003_AHA_SV_60_brass_stack_loop_in_the_city_6-8_Am.wav","./assets/sounds/musicalCube/tracks/In the City/melody/004_AHA_SV_60_vocal_loop_in_the_city_6-8_Am.wav","./assets/sounds/musicalCube/tracks/In the City/one_shots/AHA_SV_brass_stack_one_shot_stab_bump_Am.wav","./assets/sounds/musicalCube/tracks/In the City/one_shots/AHA_SV_brass_stack_one_shot_stab_choo_choo_Am.wav"]
-      },
-      { 
-        name : "New Jungle",
-        sounds : ["./assets/sounds/musicalCube/tracks/New Jungle/melody/001_Bassline_-_Dirty_Bass_170bpm_Fm_-_NEWJUNGLE_Zenhiser.wav","./assets/sounds/musicalCube/tracks/New Jungle/melody/002_Drums_-_All_Drums_170bpm_Fm_-_NEWJUNGLE_Zenhiser.wav","./assets/sounds/musicalCube/tracks/New Jungle/melody/003_Drums_-_Main_Drums_170bpm_Fm_-_NEWJUNGLE_Zenhiser.wav","./assets/sounds/musicalCube/tracks/New Jungle/melody/004_Drums_-_Top_Drums_170bpm_Fm_-_NEWJUNGLE_Zenhiser.wav","./assets/sounds/musicalCube/tracks/New Jungle/one_shots/005_Synth_Pad_170bpm_Fm_-_NEWJUNGLE_Zenhiser.wav","./assets/sounds/musicalCube/tracks/New Jungle/one_shots/006_Synth_Strings_Section_170bpm_Fm_-_NEWJUNGLE_Zenhiser.wav","./assets/sounds/musicalCube/tracks/New Jungle/one_shots/007_Synth_Cello_Section_170bpm_Fm_-_NEWJUNGLE_Zenhiser.wav","./assets/sounds/musicalCube/tracks/New Jungle/one_shots/008_Transition_FX_170bpm_Fm_-_NEWJUNGLE_Zenhiser.wav"]
-      } ];
+const tracks: Track[] = [
+  {
+    name: "Best Feelings",
+    sounds: ["./assets/sounds/musicalCube/tracks/Best Feelings/melody/001_AHA_SV_75_piano_loop_day_to_day_F.wav", "./assets/sounds/musicalCube/tracks/Best Feelings/melody/002_AHA_SV_75_keys_loop_best_feelings_F.wav", "./assets/sounds/musicalCube/tracks/Best Feelings/melody/003_AHA_SV_75_brass_loop_best_feelings_F.wav", "./assets/sounds/musicalCube/tracks/Best Feelings/melody/004_AHA_SV_75_vocal_loop_best_feelings_F.wav", "./assets/sounds/musicalCube/tracks/Best Feelings/one_shots/AHA_SV_brass_stack_one_shot_phrase_runner_up_F.wav"]
+  },
+  {
+    name: "Daydreamer",
+    sounds: ["./assets/sounds/musicalCube/tracks/Daydreamer/melody/001_AHA_SV_70_keys_loop_daydreamer_Gm.wav", "./assets/sounds/musicalCube/tracks/Daydreamer/melody/002_AHA_SV_70_guitar_stack_loop_daydreamer_Gm.wav", "./assets/sounds/musicalCube/tracks/Daydreamer/melody/003_AHA_SV_70_brass_stack_loop_daydreamer_Gm.wav", "./assets/sounds/musicalCube/tracks/Daydreamer/melody/004_AHA_SV_70_vocal_loop_daydreamer_Gm.wav"]
+  },
+  {
+    name: "In the City",
+    sounds: ["./assets/sounds/musicalCube/tracks/In the City/melody/001_AHA_SV_60_piano_loop_in_the_city_6-8_Am.wav", "./assets/sounds/musicalCube/tracks/In the City/melody/002_AHA_SV_60_guitar_stack_loop_in_the_city_6-8_Am.wav", "./assets/sounds/musicalCube/tracks/In the City/melody/003_AHA_SV_60_brass_stack_loop_in_the_city_6-8_Am.wav", "./assets/sounds/musicalCube/tracks/In the City/melody/004_AHA_SV_60_vocal_loop_in_the_city_6-8_Am.wav", "./assets/sounds/musicalCube/tracks/In the City/one_shots/AHA_SV_brass_stack_one_shot_stab_bump_Am.wav", "./assets/sounds/musicalCube/tracks/In the City/one_shots/AHA_SV_brass_stack_one_shot_stab_choo_choo_Am.wav"]
+  }];
 
 export default tracks;
