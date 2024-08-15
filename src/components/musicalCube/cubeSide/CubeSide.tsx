393bb3896f65ebf41a1bb6ac66c98feb62b38b80
Paul Visciano <paulvisciano.dev@gmail.com>
2024-08-15 19:05:36 -0400

    Move wavesurfer

--- src/components/musicalCube/cubeSide/CubeSide.tsx
+++ src/components/musicalCube/cubeSide/CubeSide.tsx
@@ -1,119 +1,27 @@
 import { IonGrid, IonRow, IonCol, IonSpinner } from '@ionic/react';
 import React, {
-    useCallback,
-    useEffect,
-    useRef,
     useState
 } from "react";
-import { WaveSurfer, WaveForm } from "wavesurfer-react";
 import CubeSideToolbar from '../toolbar/Toolbar';
 import "./CubeSide.css";
 import { Size } from '../interfaces/Size';
-import InstumentIcon from '../InstrumentIcon/InstrumentIcon';
-import { CoinLoadingIndicator } from 'components/cubeLoading/CoinLoadingIndicator';
+import WaveSurferInstance from '../waveSurferInstance/WaveSurferInstance';
 
 interface SideOptions {
     id: string;
     size: Size;
     isActive: boolean;
-    sound?: any,
+    sound: string,
     enableLoop?: boolean;
     enableSync?: boolean;
     startGlobalTimeTracker: any;
     getSharedTrackTime: any;
 };
 
-export const CubeSide: React.FC<SideOptions> = ({ id, size, isActive, enableLoop = true, enableSync = true, sound, ...props }) => {
-    //Each waveform needs to have a unique id
-    const waveFormUniqueId = `waveform-${id}`;
-    const wavesurferRef: any = useRef();
-
-    let [loading, setLoading] = useState(true);
+export const CubeSide: React.FC<SideOptions> = ({ size, isActive, enableLoop = true, ...props }) => {
     let [loop, setLoop] = useState(enableLoop);
+    let [loading, setLoading] = useState(true);
     let [showToolbar, setShowToolbar] = useState(false);
-    let [isPlaying, setIsPlaying] = useState(false);
-
-    const handleWSMount = useCallback(
-        (waveSurfer: any) => {
-            wavesurferRef.current = waveSurfer;
-            wavesurferRef.current?.load(`${process.env.PUBLIC_URL}${sound}`);
-        }, [sound]);
-
-    const onClick = (playing: boolean) => {
-        !showToolbar && setShowToolbar(true);
-
-        if (!enableSync)
-            playFromBeginning();
-        else if (playing) {
-            playPause();
-        }
-        else {
-            triggerSync();
-        }
-    }
-
-    useEffect(() => {
-        if (!wavesurferRef || !wavesurferRef.current)
-            return;
-
-        const unsubReady = wavesurferRef.current.on("ready", () => {
-            setTimeout(() => {
-                setLoading(false)
-            }, 5000)
-        }
-        );
-
-        const unsubClick = wavesurferRef.current.on("click", () => onClick(isPlaying));
-
-        const unsubFinish = wavesurferRef.current.on("finish", () => {
-            if (loop) {
-                wavesurferRef.current.seekTo(0);
-                wavesurferRef.current.play();
-                setIsPlaying(true);
-                return;
-            }
-
-            setIsPlaying(false);
-        });
-
-        return () => {
-            unsubClick();
-            unsubFinish();
-            unsubReady();
-        };
-    }, [loading, loop, isPlaying])
-
-    const playPause = () => {
-        wavesurferRef?.current?.playPause();
-
-        setIsPlaying(wavesurferRef?.current?.isPlaying());
-    }
-
-    const triggerSync = () => {
-        const sharedTrackTime = props.getSharedTrackTime();
-
-        if (sharedTrackTime) {
-            wavesurferRef.current.setTime(sharedTrackTime);
-            wavesurferRef.current.play();
-            setIsPlaying(true);
-        }
-        else {
-            wavesurferRef.current.seekTo(0);
-
-            props.startGlobalTimeTracker(() => wavesurferRef.current.getCurrentTime());
-
-            wavesurferRef.current.play();
-
-            setIsPlaying(true);
-        }
-    }
-
-    const playFromBeginning = () => {
-        wavesurferRef.current.seekTo(0);
-        wavesurferRef.current.play();
-
-        setIsPlaying(true);
-    }
 
     return (
         <div className={`cube-side ${isActive ? 'active' : ''}`}>
@@ -123,33 +31,8 @@ export const CubeSide: React.FC<SideOptions> = ({ id, size, isActive, enableLoop
                         <div style={{ height: size.height, width: size.width }}>
                             {loading && isActive && <IonSpinner className='loading-spinner' name={"lines-sharp-small"} duration={1800} />}
 
-                            <div className={`wavesurfer-custom-wrapper ${loading ? 'hide' : ''}`} onClick={() => onClick(isPlaying)}>
-                                {sound &&
-                                    <WaveSurfer
-                                        height={size.height - 4}
-                                        width={size.width - 4}
-                                        barWidth={0.5}
-                                        interact={false}
-                                        //TODO: Get these colors from colors.css
-                                        //TODO : Pass them in as params
-                                        waveColor={[
-                                            "#1976d2",
-                                            "#2196f3",
-                                            "#1976d2",
-                                        ]}
-                                        progressColor={[
-                                            "#0d47a1",
-                                        ]}
-                                        dragToSeek={false}
-                                        onMount={handleWSMount}
-                                        container={`#${waveFormUniqueId}`}
-                                    >
-                                        <WaveForm id={waveFormUniqueId} />
-                                    </WaveSurfer>
-                                }
+                            <WaveSurferInstance className={`${loading ? 'hide' : ''}`} showInstrument={isActive} size={size} loop={loop} setLoading={setLoading} setShowToolbar={setShowToolbar} {...props} />
 
-                                <InstumentIcon sound={sound} onClick={() => onClick(isPlaying)} />
-                            </div>
                         </div>
                     </IonCol>
                 </IonRow>
